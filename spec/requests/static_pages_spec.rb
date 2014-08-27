cd0db09429553f3f91d8e243f161aef54a73c489
ryousawamura <ryou_s-12a@stu.kbc.co.jp>
2014-08-27 15:10:49 +0900

    #1 5-6-2_finish

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -38,18 +38,18 @@ describe "Static pages" do
     click_link "About"
     expect(page).to have_title(full_title('About Us'))
 
-
     click_link "Help"
-    expect(page).to have_title("#{base_title}")
-    #{title_delimiter}
+    expect(page).to have_title(full_title('Help'))
 
     click_link "Contact"
-    expect(page).to # ここにコードを記入
+    expect(page).to have_title(full_title('Contact'))
+
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to # ここにコードを記入
+    expect(page).to have_title(full_title('Sign up'))
+
     click_link "sample app"
-    expect(page).to # ここにコードを記入
+    expect(page).to have_title(full_title(''))
   end
 end
 
